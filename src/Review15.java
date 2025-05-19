5bcdc47b7670751d701d3ef7699b7ac67efe0b4f
241019 <241019@std.hi-joho.ac.jp>
2025-05-19 14:54:08 +0900

    あぁ＾～いいっすねぇ＾～

---
+++ src/Review15.java
@@ -0,0 +1,5 @@
+public class Review15 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/Review22.java
@@ -0,0 +1,5 @@
+public class Review22 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
