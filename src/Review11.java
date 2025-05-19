8bd814dec9bcef3c17743b344aecfcfe3df1cca1
241019 <241019@std.hi-joho.ac.jp>
2025-05-19 12:10:23 +0900

    ✞悔い改めて✞

--- src/Review11.java
+++ src/Review11.java
@@ -1,5 +1,10 @@
 public class Review11 {
     public static void main(String[] args) {
-        System.out.println();
+        int num = 114514;
+        if((num / 10) != 0){
+            System.out.println("この数は10で割り切れない値です。");
+        }else{
+            System.out.println("この数は10で割り切れる値です。");
+        }
     }
 }
