a5f03468911e309d49586a9bbd109fe85d458204
241019 <241019@std.hi-joho.ac.jp>
2025-05-19 12:18:11 +0900

    まずうちさぁ、

--- src/Review12.java
+++ src/Review12.java
@@ -1,5 +1,16 @@
 public class Review12 {
     public static void main(String[] args) {
-        System.out.println();
+        int age = 114514;
+        if (age >= 0) {
+            if(age <= 17){
+                System.out.println("未成年");
+            } else if (age <= 64) {
+                System.out.println("成年");
+            }else{
+                System.out.println("高齢者");
+            }
+        }else{
+            System.out.println("無効な年齢です。");
+        }
     }
 }
