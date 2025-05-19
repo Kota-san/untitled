2e480dafac7e97708dac1e11c947ed3ffd749c59
241019 <241019@std.hi-joho.ac.jp>
2025-05-19 11:43:29 +0900

    すっごいおっきい...

--- src/Review8.java
+++ src/Review8.java
@@ -1,5 +1,12 @@
 public class Review8 {
     public static void main(String[] args) {
-        System.out.println();
+        double a = 10;
+        double b = a;
+        a = b;
+        a = 10000;
+        b = 10000L;
+        b = (int)a;
+        float f = 5.94f;
+        short s = 10;
     }
 }
