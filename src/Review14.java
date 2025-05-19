a0f0a34881d6afe3ebdd21f968091f885d5049e5
241019 <241019@std.hi-joho.ac.jp>
2025-05-19 12:26:40 +0900

    焼いてかない？

--- src/Review14.java
+++ src/Review14.java
@@ -1,5 +1,14 @@
 public class Review14 {
     public static void main(String[] args) {
-        System.out.println();
+        String color = "赤";
+        if (color.equals("赤")) {
+            System.out.println("停止");
+        }else if (color.equals("黄")) {
+            System.out.println("注意");
+        }else if (color.equals("青")) {
+            System.out.println("進行");
+        }else{
+            System.out.println("無効な信号の色です。");
+        }
     }
 }
