0bd692e522d5ca41b342567040240559993ab0a0
241019 <241019@std.hi-joho.ac.jp>
2025-05-23 11:11:54 +0900

    喉乾いた...喉乾かない？

--- src/reviews3/Review28.java
+++ src/reviews3/Review28.java
@@ -1,4 +1,7 @@
 package reviews3;
 
 public class Review28 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
--- src/reviews3/Review29.java
+++ src/reviews3/Review29.java
@@ -1,4 +1,7 @@
 package reviews3;
 
 public class Review29 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
