60b84bb6bb4efe0770a114607ed47732183fe01b
241019 <241019@std.hi-joho.ac.jp>
2025-05-23 11:58:22 +0900

    サーッ！（迫真）

--- src/reviews2/Review15.java
+++ src/reviews2/Review15.java
@@ -2,6 +2,15 @@ package reviews2;
 
 public class Review15 {
     public static void main(String[] args) {
-        System.out.println();
+        int num = 114514;
+        if (num > 0) {
+            if(num % 2 == 0) {
+                System.out.println("偶数");
+            } else{
+                System.out.println("奇数");
+            }
+        }else{
+            System.out.println("無効な入力です。");
+        }
     }
 }
--- src/reviews2/Review19.java
+++ src/reviews2/Review19.java
@@ -2,6 +2,13 @@ package reviews2;
 
 public class Review19 {
     public static void main(String[] args) {
-        System.out.println();
+        int num = 10;
+        int factorial = 1;
+        int i = 1;
+        while (i < num) {
+            factorial = factorial * i;
+            i = i + 1;
+        }
+        System.out.println("結果" + factorial);
     }
 }
